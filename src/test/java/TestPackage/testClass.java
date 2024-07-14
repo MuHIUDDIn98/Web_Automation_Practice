6661a11120317e2b615d6364f1454349ea953405
Muhiuddin anik <muhiuddinanik98@gmail.com>
2024-07-14 14:26:35 +0600

    someBasic config file changes

---
+++ src/test/java/TestPackage/testClass.java
@@ -0,0 +1,19 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class testClass {
+	
+	@Test
+	public void Demo() {
+		
+		System.out.println("Hello");
+	}
+	
+	@Test 
+	public void Demo2() {
+		System.out.println("Hello2");
+		
+	}
+
+}
